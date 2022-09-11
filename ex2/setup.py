42c2a90e47245e482d8c531d10a67def1c316097
KerenRozen <92442665+KerenRozen@users.noreply.github.com>
2022-09-11 13:09:34 +0300

    Add files via upload

--- ex2/setup.py
+++ ex2/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, Extension
 setup(
     name= "mykmeanssp",
     version="0.1.0",
-    author="Roni Meamen, Keren Rozen",
+    author="Keren Rozen",
     description="C-API for kmeans",
     install_requires=['invoke'],
     packages=find_packages(),
